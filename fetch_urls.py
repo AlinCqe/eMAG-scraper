4cf3f32a671b265363d9f0f66d64c19c364cceb8
AlinCqe <alinemanuelv@gmail.com>
2025-06-20 16:11:23 +0300

    Working script
    Works perfectly for now.
    
    Next upgrades is doing some kind of data filter and trying to make it faster

---
+++ fetch_urls.py
@@ -0,0 +1,96 @@
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
+from selenium.webdriver.chrome.service import Service
+import time
+import os
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument("--log-level=3")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+service = Service(log_path=os.devnull)
+    
+
+
+def fetch_urls(search_item):
+    print('Starting url fetcher')
+    driver = webdriver.Chrome(service=service,options=options)
+    wait = WebDriverWait(driver, 10)
+
+    search_item_formated = search_item.strip().replace(' ', '+')
+    
+    driver.get(f'https://www.emag.ro/search/{search_item_formated}')
+    first_hidden_api = None
+
+
+    # Gets the first hidden api 
+    while not first_hidden_api:
+        for request in driver.requests:
+            if request.response:
+                if request.url.startswith('https://sapi.emag.ro/recommendations/by-zone-by-filters'):
+                    first_hidden_api = f'{request.url}'
+                    print('Firts URL catched')
+        driver.refresh()
+        time.sleep(1)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'has-chat-button')))
+
+    # Set the page items limit to max so we dont have to loop trought the same page
+    suffix = f'page%5Boffset%5D=0&page%5Blimit%5D=12'
+    first_hidden_api = first_hidden_api.replace(suffix, f'page%5Boffset%5D=0&page%5Blimit%5D=100')
+
+    # Gets the second hidden api
+    for _ in range(5):
+        try:
+            for request in driver.requests:
+                if request.response.status_code == 200:
+                    if request.url.startswith('https://www.emag.ro/search-by-url?source_id='):
+                        second_hidden_api = f'{request.url}'
+                        print('Second URL catched')
+                        break                        
+
+            # Try clicking the cookies thing
+            try:
+                firts_cookies_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME , 'btn.btn-primary.btn-block.js-accept.gtm_h76e8zjgoo')))
+                firts_cookies_button.click()
+            except (StaleElementReferenceException,TimeoutException):
+                print('No "Close cookies pop-up" button was found')
+                continue
+
+            # Try closing the login thing
+            try:
+                close_loggin_notif_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME , 'js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.py-0.px-0')))
+                close_loggin_notif_button.click()
+            except (StaleElementReferenceException, TimeoutException):
+                print('No "Close login pop-up" button was found')
+                continue
+
+            # Try clicking next page button
+            try:
+                next_page_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Next']")))
+                next_page_button.click()
+            except (StaleElementReferenceException, TimeoutException):
+                print('No "Next" button was found')
+                break
+
+        # The request fetch/xhr url might be None, this catch the error, reload the page and continues
+        except AttributeError:
+            print('Couldnt find the seccond URL. Reload the page')
+            driver.refresh()
+            time.sleep(1)
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'has-chat-button')))
+
+
+    driver.quit()
+    
+    return first_hidden_api, second_hidden_api
+
+
+
+
+if __name__ == '__main__':
+    fetch_urls()
--- main.py
+++ main.py
@@ -1,204 +1,14 @@
-import requests
-import time
-from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
-import random
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
-from requests.exceptions import JSONDecodeError
-import pandas as pd
-import os
-
-
-options = Options()
-options.add_argument('--headless')
-
-
-search_item = str(input('What item do you want to search for? '))
-search_item_formated = search_item.strip().replace(' ', '+')
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,es;q=0.7,ro;q=0.6',
-    'Referer': 'https://google.ro/',
-    'DNT': '1',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Connection': 'keep-alive'
-}
-
-driver = webdriver.Chrome(options=options)
-driver.get(f'https://www.emag.ro/search/{search_item_formated}')
-print(f'https://www.emag.ro/search/{search_item_formated}')
-
-
-wait = WebDriverWait(driver, 10)
-
-
-def get_first_hidden_api():
-    first_hidden_api = None
-    # Gets the first hidden api 
-    while not first_hidden_api:
-        for request in driver.requests:
-            if request.response:
-                if request.url.startswith('https://sapi.emag.ro/recommendations/by-zone-by-filters'):
-                    first_hidden_api = f'{request.url}'
-        driver.refresh()
-        time.sleep(1)
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'has-chat-button')))
-
-    # Set the page items limit to max so we dont have to loop trought the same page
-    suffix = f'page%5Boffset%5D=0&page%5Blimit%5D=12'
-    first_hidden_api = first_hidden_api.replace(suffix, f'page%5Boffset%5D=0&page%5Blimit%5D=100')
-    return first_hidden_api
+from fetch_urls import fetch_urls
+from data_scraper import data_extract_first_api, data_extract_second_api, html_scraper
 
 
 
+search_item = input('What item do you want to search for?: ')
+first_url, second_url = fetch_urls(search_item)
 
-# Gets the second hidden api that contains more data
-def get_second_hidden_api():
-    for _ in range(5):
-        try:
-            for request in driver.requests:
-                if request.response.status_code == 200:
-                    if request.url.startswith('https://www.emag.ro/search-by-url?source_id='):
-                        second_hidden_api = f'{request.url}'
-                        return second_hidden_api
-                
-            
-            # Try clicking the cookies thing
-            try:
-                firts_cookies_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME , 'btn.btn-primary.btn-block.js-accept.gtm_h76e8zjgoo')))
-                firts_cookies_button.click()
-            except (StaleElementReferenceException,TimeoutException):
-                print('No "Close cookies pop-up" button was found')
-                continue
-
-            # Try closing the login thing
-            try:
-                close_loggin_notif_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME , 'js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.py-0.px-0')))
-                close_loggin_notif_button.click()
-            except (StaleElementReferenceException, TimeoutException):
-                print('No "Close login pop-up" button was found')
-                continue
-
-
-            try:
-                next_page_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Next']")))
-                next_page_button.click()
-            except (StaleElementReferenceException, TimeoutException):
-                print('No "Next" button was found')
-                break
-
-        # The request fetch/xhr url might be None, this catch the error, reload the page and continues
-        except AttributeError:
-            print('Couldnt find the seccond URL. Reload the page')
-            driver.refresh()
-            time.sleep(1)
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'has-chat-button')))
-
-
-first_hidden_api = get_first_hidden_api()
-second_hidden_api = get_second_hidden_api()
-
-driver.quit()
-
-
-
-fetch = []
-items_printed_count = 0
-
-items_id_used_set = set()
-def data_extract_first_api(api):
-    global items_printed_count
-    global fetch
-    
-    # Calls the first hidden api and prints data until there is no more data, raise a key error and exits the program
-    response = requests.get(api, headers=headers)
-    data = response.json()
-    items = data["data"]['adss']['product_collection']
-    for item in items:
-        # Check if item id hasnt been used/printed
-        if item['id'] in items_id_used_set:
-            continue
-
-        item_name = item['name']
-        item_price = item['offer']['price']['current']
-        item_currency = item['offer']['price']['currency']['name']['default']
-        #print(item_name, item_currency, item_price)
-        items_printed_count += 1
-
-        fetch.append({'item_name': item_name, 'item_price': item_price, 'item_currency': item_currency})
-        items_id_used_set.add(item['id'])
-        
-        if len(fetch) >= 200:
-            df = pd.DataFrame(fetch) 
-            df.to_csv('data.csv', mode='a', index=False, header=not os.path.exists('data.csv') )
-            fetch = []
-
-
-
-data_extract_first_api(first_hidden_api)
-
-if second_hidden_api is not None:
-# Gets the second json file with more data and prints items + prices using second hidden url
-    page_number = 2
-    try:
-        while True:
-            
-            response = requests.get(second_hidden_api, headers=headers)
-            data = response.json()
-            items = data['data']['items']
-
-            
-            for item in items:
-
-                if item['id'] in items_id_used_set:
-                    continue
-                items_id_used_set.add(item['id'])
-
-                
-                item_name = item['name']
-                item_price = item['offer']['price']['current']
-                item_currency = item['offer']['price']['currency']['name']['default']
-                
-                
-                
-                fetch.append({'item_name': item_name, 'item_price': item_price, 'item_currency': item_currency})
-                items_printed_count += 1
-                if len(fetch) >= 200:
-                    df = pd.DataFrame(fetch) 
-                    df.to_csv('data.csv', mode='a', index=False, header=not os.path.exists('data.csv') )
-                    fetch = []
-
-
-            # Goes to the next page
-            second_hidden_api = second_hidden_api.replace(fr'%2Fp{page_number}&', fr'%2Fp{page_number + 1}&')
-
-            page_number += 1
-            time.sleep(random.randint(1,5))
-       
-    except JSONDecodeError:
-        pass
-
-df = pd.DataFrame(fetch) 
-df.to_csv('data.csv', mode='a', index=False, header=not os.path.exists('data.csv') )
-    
-print(f'Script runned succsesfully with a total of {items_printed_count} items printed')
-
-
-
-
-
-
-
-
-
-
-
-
-
+html_scraper(search_item)
+data_extract_first_api(first_url)
+data_extract_second_api(second_url)
 
 
 ## FUNCIONA LO DE CATCH LAS URL, AHORA REQUEST ESAS URLS I RECOGER DATA DE ELLAS - done
@@ -215,16 +25,24 @@ print(f'Script runned succsesfully with a total of {items_printed_count} items p
 
 # despues de hacer le funcion para la segunda api emepzar a guradar datos - done
 
-# catch error, buscando g29 no tengo muchos items y no hay boton de next, cazar cuando da ese error posible mente con un simple try catchs - hecho creo/ revisar mas terde
+# catch error, buscando g29 no tengo muchos items y no hay boton de next, cazar cuando da ese error posible mente con un simple try catchs - hecho creo/ revisar mas terde  - done
 
-# PROBLEMA !!! - hay item estaticos tmb, unos tmb con top favirtes que pueden venir de otra parte, no del endpoint
-#puedo usar html scraper primero, y luego las dos hiddens api
-#filtrar datos con id(lo que uso ahora)
-#empezar a ordenar codigo ahora. es un poco messy
+# PROBLEMA !!! - hay item estaticos tmb, unos tmb con top favirtes que pueden venir de otra parte, no del endpoint - done
+#puedo usar html scraper primero, y luego las dos hiddens api - done
+#filtrar datos con id(lo que uso ahora) - done
+
+#empezar a ordenar codigo ahora. es un poco messy - he separado la logica - main llama pide input y llama todo - fethc url consige las hidden api - json scraper recoge los datos de la funciones - done
+
+# en el de recoger apis, el driver se ejetuca primero y no quiero, quiero que lo haga despues o talvez desactivar el texto y ya(mas facil chat gpt te ayuda) - no puedo, hacer wait en main y ya  - done
+
+# prodria hacer el html scaper en su file y importarla a json scraper/talvez generalizar el file con data extract y meter las tres logicas ahi - parcialmente done creo
+
+# necesito ir guardando las id de los itmes en las tres functiones, ese es el motivo para hacer esto - done creo
+
+#tmb hacer que no printee cosas el selenium - no soy capaz. resuelto de otra manera - done
 
 
-
-
+#mejorar el fetch de la segunda api, no esta bien hecha - en proceso
 
 
 # crear nombre del archivo con el texto de la busceda, facil de hacer
@@ -241,3 +59,4 @@ Got firts hidden api
 got second hidden api
 running data excart with firts api 
 '''
+
